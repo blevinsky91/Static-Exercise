fbdb6801acf6e6defdf65299088b0c3f83452757
Brandon Levinsky <blevinsky91@gmail.com>
2023-07-05 15:00:46 -0400

    ex1 complete

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            var celsius = TempConverter.FahrenheitToCelsius(50.00); 
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(100.00);
+
+            Console.WriteLine($"{fahrenheit} degrees fahrenheit");
+            Console.WriteLine($"{celsius} degrees celsius");
         }
     }
 }
